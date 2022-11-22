83a4675fb077e665a17ba81459637d4616ac91e0
Siarhei Konau <konausiarhei@gmail.com>
2022-11-22 14:47:30 +0300

    create module

---
+++ ScnBatchSize/CalculateRecomendetBatchSize/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ScnBatchSize_CalculateRecomendetBatchSize',
+    __DIR__
+);
