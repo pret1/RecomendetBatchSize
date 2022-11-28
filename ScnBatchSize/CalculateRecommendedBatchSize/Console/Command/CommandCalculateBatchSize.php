33c3ec7c341b6ec598d1588037620c95f4721bff
Siarhei Konau <konausiarhei@gmail.com>
2022-11-28 10:11:39 +0300

    Moved logic in model

--- ScnBatchSize/CalculateRecommendedBatchSize/Console/Command/CommandCalculateBatchSize.php
+++ ScnBatchSize/CalculateRecommendedBatchSize/Console/Command/CommandCalculateBatchSize.php
@@ -8,30 +8,24 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Command\Command;
-use Magento\Framework\Indexer\IndexTableRowSizeEstimatorInterface;
-use Magento\Framework\App\ResourceConnection;
+use ScnBatchSize\CalculateRecommendedBatchSize\Model\CalculateBatchSize;
 
 class CommandCalculateBatchSize extends Command
 {
     public const COMMAND_NAME = 'calculate:batch:size';
 
-    private ResourceConnection $resourceConnection;
-
     /**
-     * @var IndexTableRowSizeEstimatorInterface[]
+     * @param CalculateBatchSize $calculateBatchSize
      */
-    private array $rowSizeEstimatorPool;
-
     public function __construct(
-        ResourceConnection $resourceConnection,
-        array $rowSizeEstimatorPool = [],
-        string $name = null,
+        private readonly CalculateBatchSize $calculateBatchSize
     ) {
-        parent::__construct($name);
-        $this->resourceConnection = $resourceConnection;
-        $this->rowSizeEstimatorPool = $rowSizeEstimatorPool;
+        parent::__construct();
     }
 
+    /**
+     * @return void
+     */
     protected function configure(): void
     {
         $this->setName(self::COMMAND_NAME);
@@ -41,28 +35,12 @@ class CommandCalculateBatchSize extends Command
     }
 
     /**
-     * Execute the command
-     *
      * @param InputInterface $input
      * @param OutputInterface $output
-     *
-     * @return null|int
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @return void
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $connection = $this->resourceConnection->getConnection();
-
-        foreach ($this->rowSizeEstimatorPool as $nameIndex => $rowSizeEstimator) {
-            $rowMemory = $rowSizeEstimator->estimateRowSize();
-            $bufferPoolSize = $connection->fetchOne('SELECT @@innodb_buffer_pool_size;');
-            $batchSize = ceil((($bufferPoolSize * 0.2) / $rowMemory) * 0.95);
-            echo sprintf(
-                "Command %s work, recommended batchSize=%s for index %s \n",
-                self::COMMAND_NAME,
-                $batchSize,
-                $nameIndex
-            );
-        }
+        $this->calculateBatchSize->execute();
     }
 }
--- ScnBatchSize/CalculateRecommendedBatchSize/Model/CalculateBatchSize.php
+++ ScnBatchSize/CalculateRecommendedBatchSize/Model/CalculateBatchSize.php
@@ -1,35 +1,42 @@
 <?php
-//
-//declare(strict_types=1);
-//
-//namespace ScnBatchSize\CalculateRecommendedBatchSize\Model;
-//
-//class CalculateBatchSize
-//{
-//    public function __construct(
-//        private readonly IndexTableRowSizeEstimatorInterface $rowSizeEstimator,
-//        private readonly AdapterInterface $connectionTwo,
-//        ResourceConnection $resource,
-//        StoreManagerInterface $storeManager,
-//        Config $config,
-//    ) {
-//        parent::__construct(
-//            $resource,
-//            $storeManager,
-//            $config,
-//        );
-//    }
-//    public function execute()
-//    {
-//         $this->connection;
-//        $this->calculateBitchSize($this->connection);
-//    }
-//
-//    public function calculateBitchSize(\Magento\Framework\DB\Adapter\AdapterInterface $connectionTwo)
-//    {
-//        $rowMemory = $this->rowSizeEstimator->estimateRowSize();
-//        $bufferPoolSize = $this->connectionTwo->fetchOne('SELECT @@innodb_buffer_pool_size;');
-//
-//       return $batchSize = (($bufferPoolSize * 0.2)/$rowMemory)*0.95;
-//    }
-//}
+
+declare(strict_types=1);
+
+namespace ScnBatchSize\CalculateRecommendedBatchSize\Model;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Indexer\IndexTableRowSizeEstimatorInterface;
+
+class CalculateBatchSize
+{
+    private ResourceConnection $resourceConnection;
+
+    /**
+     * @var IndexTableRowSizeEstimatorInterface[]
+     */
+    private array $rowSizeEstimatorPool;
+
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        array $rowSizeEstimatorPool = [],
+    ) {
+        $this->resourceConnection = $resourceConnection;
+        $this->rowSizeEstimatorPool = $rowSizeEstimatorPool;
+    }
+
+    public function execute(): void
+    {
+        $connection = $this->resourceConnection->getConnection();
+
+        foreach ($this->rowSizeEstimatorPool as $nameIndex => $rowSizeEstimator) {
+            $rowMemory = $rowSizeEstimator->estimateRowSize();
+            $bufferPoolSize = $connection->fetchOne('SELECT @@innodb_buffer_pool_size;');
+            $batchSize = ceil((($bufferPoolSize * 0.2) / $rowMemory) * 0.95);
+            echo sprintf(
+                "Recommended batchSize=%s for index %s \n",
+                $batchSize,
+                $nameIndex
+            );
+        }
+    }
+}
